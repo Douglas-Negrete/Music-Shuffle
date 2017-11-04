574a507eace7c8a5a9cbf7c134f064212732b802
ama82 <ama82@Douglas>
2017-11-03 21:42:16 -0400

    This is a test to make sure that the jar lib that i am using will work

---
+++ Shuffle/src/Main.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		String ar;
+
+		File file = new File("Bastille - Warmth.mp3");
+		AudioFile f;
+		try {
+			
+			f = AudioFileIO.read(file);
+			Tag tag = f.getTag();
+			ar = tag.getFirst(FieldKey.ARTIST);
+			System.out.println(ar);
+			
+		} catch (CannotReadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TagException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ReadOnlyFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidAudioFrameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}//end main
+
+}//end class
